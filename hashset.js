a9d51f85a4ebb1263c4f7309212bf05107aefd1e
Shuping <lsp.lhh@gmail.com>
2015-06-18 23:25:05 +0800

    take an array parameter as a single value

--- hashset.js
+++ hashset.js
@@ -1,142 +1,133 @@
 var hash = require('string-hash');
 
-function HashSet(init) {
-	var length = 0,
-		self = this,
-		map = Object.create(null);
+function HashSet() {
+    var length = 0,
+        self = this,
+        map = Object.create(null);
 
-	Object.defineProperty(this, 'length', {
-		get: function() {
-			return length;
-		}
-	});
+    Object.defineProperty(this, 'length', {
+        get: function() {
+            return length;
+        }
+    });
 
-	Object.defineProperty(this, 'values', {
-		get: function() {
-			var subkeys;
-			var values = [];
-			var keys = Object.keys(map);
-			keys.forEach(function(key) {
-				subkeys = Object.keys(map[key]);
-				subkeys.forEach(function(subkey) {
-					values.push(map[key][subkey]);
-				});
-			});
+    Object.defineProperty(this, 'values', {
+        get: function() {
+            var subkeys;
+            var values = [];
+            var keys = Object.keys(map);
+            keys.forEach(function(key) {
+                subkeys = Object.keys(map[key]);
+                subkeys.forEach(function(subkey) {
+                    values.push(map[key][subkey]);
+                });
+            });
 
-			return values;
-		}
-	});
+            return values;
+        }
+    });
 
-	if (toString.call(init) == '[object Array]') {
-		init.forEach(function(v) {
-			add(v);
-		});
-	} else if (arguments.length > 1) {
-		[].forEach.call(arguments, function(v) {
-			console.log('arguments add item: ', v, length);
-			add(v);
-		});
-	} else if (arguments.length === 1) {
-		add(init);
-	}
+    [].forEach.call(arguments, function(v) {
+        add(v);
+    });
 
-	this.contains = function(val) {
-		var type = toString.call(val);
-		if (map[type] === undefined) {
-			return false;
-		}
+    this.contains = function(val) {
+        var type = toString.call(val);
+        if (map[type] === undefined) {
+            return false;
+        }
 
-		var key = hash('' + val);
-		return map[type][key] !== undefined;
-	};
+        var key = hash('' + val);
+        return map[type][key] !== undefined;
+    };
 
-	function add(val) {
-		var type = toString.call(val);
-		if (map[type] === undefined) {
-			map[type] = Object.create(null);
-		}
+    function add(val) {
+        var type = toString.call(val);
+        if (map[type] === undefined) {
+            map[type] = Object.create(null);
+        }
 
-		var key = hash('' + val);
-		if (map[type][key] === undefined) {
-			map[type][key] = val;
-			length++;
-		}
-	};
+        var key = hash('' + val);
+        if (map[type][key] === undefined) {
+            map[type][key] = val;
+            length++;
+        }
+    };
 
-	this.add = add;
+    this.add = add;
 
-	this.remove = function(val) {
-		var type = toString.call(val);
-		if (map[type] === undefined) {
-			return;
-		}
+    this.remove = function(val) {
+        var type = toString.call(val);
+        if (map[type] === undefined) {
+            return;
+        }
 
-		var key = hash('' + val);
-		if (map[type][key] !== undefined) {
-			map[type][key] = undefined;
-			length--;
-		}
-	};
+        var key = hash('' + val);
+        if (map[type][key] !== undefined) {
+            map[type][key] = undefined;
+            length--;
+        }
+    };
 
-	this.isSubSetOf = function(hashset) {
-		if (length < 1) {
-			return true;
-		}
+    this.isSubSetOf = function(hashset) {
+        if (length < 1) {
+            return true;
+        }
 
-		if (hashset.length < 1) {
-			return false;
-		}
+        if (hashset.length < 1) {
+            return false;
+        }
 
-		var values = self.values;
-		for (var i in values) {
-			if (!hashset.contains(values[i])) {
-				return false;
-			}
-		}
+        var values = self.values;
+        for (var i in values) {
+            if (!hashset.contains(values[i])) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	this.isSuperSetOf = function(hashset) {
-		if (hashset.length === 0) {
-			return true;
-		}
+    this.isSuperSetOf = function(hashset) {
+        if (hashset.length === 0) {
+            return true;
+        }
 
-		if (this.length < hashset.length) {
-			return false;
-		}
+        if (this.length < hashset.length) {
+            return false;
+        }
 
-		var values = hashset.values;
-		for (var i in values) {
-			if (!self.contains(values[i])) {
-				return false;
-			}
-		}
+        var values = hashset.values;
+        for (var i in values) {
+            if (!self.contains(values[i])) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	this.unionWith = function(hashset) {
-		var values = hashset.values;
-		values.forEach(function(v) {
-			self.add(v);
-		});
-	}
+    this.unionWith = function(hashset) {
+        var values = hashset.values;
+        values.forEach(function(v) {
+            self.add(v);
+        });
+    }
 
-	this.equals = function(hashset) {
-		if (self.length !== hashset.length) {
-			return false;
-		}
+    this.equals = function(hashset) {
+        if (self.length !== hashset.length) {
+            return false;
+        }
 
-		var values = hashset.values;
-		for (var i in values) {
-			if (!self.contains(values[i])) {
-				return false;
-			}
-		}
+        var values = hashset.values;
+        for (var i in values) {
+            if (!self.contains(values[i])) {
+                return false;
+            }
+        }
 
-		return true;
-	};
+        return true;
+    };
 }
 
 module.exports = HashSet;
--- test/hashset.test.js
+++ test/hashset.test.js
@@ -1,182 +1,183 @@
 var assert = require('assert');
 var HashSet = require('../hashset.js');
 
+describe('constructor', function() {
+    it('should initialze an array as a single value.', function() {
+        var arr = [1, 2, 3, 4];
+        var hashset = new HashSet(arr);
+        assert.strictEqual(hashset.length, 1);
+        assert.strictEqual(hashset.contains(arr), true);
+    });
+});
+
 describe('#contains', function() {
-	it('should return false if element is not added yet.', function() {
-		var hashset = new HashSet();
-		assert.strictEqual(hashset.contains('a'), false);
-	});
+    it('should return false if element is not added yet.', function() {
+        var hashset = new HashSet();
+        assert.strictEqual(hashset.contains('a'), false);
+    });
 
-	it('should return true if element is added.', function() {
-		var hashset = new HashSet('a');
-		assert.strictEqual(hashset.contains('a'), true);
+    it('should return true if element is added.', function() {
+        var hashset = new HashSet('a');
+        assert.strictEqual(hashset.contains('a'), true);
 
-		hashset = new HashSet();
-		hashset.add('a');
-		assert.strictEqual(hashset.contains('a'), true);
+        hashset = new HashSet();
+        hashset.add('a');
+        assert.strictEqual(hashset.contains('a'), true);
 
-		hashset = new HashSet(1, 2, 3);
-		assert.strictEqual(hashset.contains(2), true);
-	});
+        hashset = new HashSet(1, 2, 3);
+        assert.strictEqual(hashset.contains(2), true);
+    });
 
-	it('should initialze with all elements in the given array.', function() {
-		var hashset = new HashSet([1, 2, 3, 4]);
-		assert.strictEqual(hashset.contains(1), true);
-		assert.strictEqual(hashset.contains(2), true);
-		assert.strictEqual(hashset.contains(3), true);
-		assert.strictEqual(hashset.contains(4), true);
-	});
+    it('should differentiate string and number', function() {
+        var hashset = new HashSet(1);
+        hashset.add('1');
+        assert.strictEqual(hashset.contains(1), true);
+        assert.strictEqual(hashset.contains('1'), true);
+    });
 
-	it('should differentiate string and number', function() {
-		var hashset = new HashSet(1);
-		hashset.add('1');
-		assert.strictEqual(hashset.contains(1), true);
-		assert.strictEqual(hashset.contains('1'), true);
-	});
-
-	it('should compare reference types', function() {
-		var arr = [1, 2, 3, 4];
-		var hashset = new HashSet();
-		hashset.add(arr);
-		assert.strictEqual(hashset.contains(arr), true);
-	});
+    it('should compare reference types', function() {
+        var arr = [1, 2, 3, 4];
+        var hashset = new HashSet();
+        hashset.add(arr);
+        assert.strictEqual(hashset.contains(arr), true);
+    });
 });
 
 describe('#remove', function() {
-	it('should not throw error when removing an no-existing element.', function() {
-		var hashset = new HashSet();
-		assert.doesNotThrow(function() {
-			hashset.remove('a')
-		});
-	});
+    it('should not throw error when removing an no-existing element.', function() {
+        var hashset = new HashSet();
+        assert.doesNotThrow(function() {
+            hashset.remove('a');
+        });
+    });
 });
 
 describe('#length', function() {
-	it('should be readonly property', function() {
-		var hashset = new HashSet(1, 2, 3);
-		assert.strictEqual(hashset.length, 3);
-		hashset.length = 100;
-		assert.strictEqual(hashset.length, 3);
-	});
+    it('should be readonly property', function() {
+        var hashset = new HashSet(1, 2, 3);
+        assert.strictEqual(hashset.length, 3);
+        hashset.length = 100;
+        assert.strictEqual(hashset.length, 3);
+    });
 });
 
 describe('#isSubSetOf', function() {
-	it('should return true if left side is an empty hashset', function() {
-		var hashset1 = new HashSet();
-		var hashset2 = new HashSet();
-		var result = hashset1.isSubSetOf(hashset2);
-		assert.strictEqual(result, true);
+    it('should return true if left side is an empty hashset', function() {
+        var hashset1 = new HashSet();
+        var hashset2 = new HashSet();
+        var result = hashset1.isSubSetOf(hashset2);
+        assert.strictEqual(result, true);
 
-		hashset2.add(4, 5);
-		result = hashset1.isSubSetOf(hashset2);
-		assert.strictEqual(result, true);
-	});
+        hashset2.add(4, 5);
+        result = hashset1.isSubSetOf(hashset2);
+        assert.strictEqual(result, true);
+    });
 
-	it('should return false if right side is empty hashset and left side not', function() {
-		var hashset1 = new HashSet(1);
-		var hashset2 = new HashSet();
-		var result = hashset1.isSubSetOf(hashset2);
-		assert.strictEqual(result, false);
-	});
+    it('should return false if right side is empty hashset and left side not', function() {
+        var hashset1 = new HashSet(1);
+        var hashset2 = new HashSet();
+        var result = hashset1.isSubSetOf(hashset2);
+        assert.strictEqual(result, false);
+    });
 
-	it('should return false if right side has value not existing in lef side', function() {
-		var hashset1 = new HashSet(1, 2, 3);
-		var hashset2 = new HashSet(1, 2, 4);
-		var result = hashset1.isSubSetOf(hashset2);
-		assert.strictEqual(result, false);
-	});
+    it('should return false if right side has value not existing in lef side', function() {
+        var hashset1 = new HashSet(1, 2, 3);
+        var hashset2 = new HashSet(1, 2, 4);
+        var result = hashset1.isSubSetOf(hashset2);
+        assert.strictEqual(result, false);
+    });
 });
 
 describe('#isSuperSetOf', function() {
-	it('should return true if right side is an empty hashset', function() {
-		var hashset1 = new HashSet();
-		var hashset2 = new HashSet();
-		var result = hashset1.isSuperSetOf(hashset2);
-		assert.strictEqual(result, true);
+    it('should return true if right side is an empty hashset', function() {
+        var hashset1 = new HashSet();
+        var hashset2 = new HashSet();
+        var result = hashset1.isSuperSetOf(hashset2);
+        assert.strictEqual(result, true);
 
-		hashset1.add(2);
-		result = hashset1.isSuperSetOf(hashset2);
-		assert.strictEqual(result, true);
-	});
+        hashset1.add(2);
+        result = hashset1.isSuperSetOf(hashset2);
+        assert.strictEqual(result, true);
+    });
 
-	it('should return true if left side and right side are identical', function() {
-		var hashset1 = new HashSet(1, 2, 3);
-		var hashset2 = new HashSet(1, 2, 3);
-		var result1 = hashset1.isSuperSetOf(hashset2);
-		var result2 = hashset2.isSuperSetOf(hashset1);
+    it('should return true if left side and right side are identical', function() {
+        var hashset1 = new HashSet(1, 2, 3);
+        var hashset2 = new HashSet(1, 2, 3);
+        var result1 = hashset1.isSuperSetOf(hashset2);
+        var result2 = hashset2.isSuperSetOf(hashset1);
 
-		assert.strictEqual(result1, true);
-		assert.strictEqual(result2, true);
-	});
+        assert.strictEqual(result1, true);
+        assert.strictEqual(result2, true);
+    });
 
-	it('should return true if left side contains all elements from right side', function() {
-		var hashset1 = new HashSet(1, 2, 3);
-		var hashset2 = new HashSet(2, 3);
-		var result1 = hashset1.isSuperSetOf(hashset2);
-		var result2 = hashset2.isSuperSetOf(hashset1);
+    it('should return true if left side contains all elements from right side', function() {
+        var hashset1 = new HashSet(1, 2, 3);
+        var hashset2 = new HashSet(2, 3);
+        var result1 = hashset1.isSuperSetOf(hashset2);
+        var result2 = hashset2.isSuperSetOf(hashset1);
 
-		assert.strictEqual(result1, true);
-		assert.strictEqual(result2, false);
-	});
+        assert.strictEqual(result1, true);
+        assert.strictEqual(result2, false);
+    });
 
-	it('should return false if right side hash set has value not existing in left side', function() {
-		var hashset1 = new HashSet(1, 2, 3);
-		var hashset2 = new HashSet(2, 3, 4);
-		var result = hashset1.isSuperSetOf(hashset2);
-		assert.strictEqual(result, false);
-	});
+    it('should return false if right side hash set has value not existing in left side', function() {
+        var hashset1 = new HashSet(1, 2, 3);
+        var hashset2 = new HashSet(2, 3, 4);
+        var result = hashset1.isSuperSetOf(hashset2);
+        assert.strictEqual(result, false);
+    });
 });
 
 describe('#equals', function() {
-	it('should return true when testing 2 empty hash sets', function() {
-		var hashset1 = new HashSet();
-		var hashset2 = new HashSet();
-		var result = hashset1.equals(hashset2);
-		result = result && hashset2.equals(hashset1);
-		assert.strictEqual(result, true);
-	});
+    it('should return true when testing 2 empty hash sets', function() {
+        var hashset1 = new HashSet();
+        var hashset2 = new HashSet();
+        var result = hashset1.equals(hashset2);
+        result = result && hashset2.equals(hashset1);
+        assert.strictEqual(result, true);
+    });
 
-	it('should return false when lengths of 2 hash sets do not match', function() {
-		var hashset1 = new HashSet();
-		var hashset2 = new HashSet(1);
-		var result = hashset1.equals(hashset2);
-		assert.strictEqual(result, false);
-	});
+    it('should return false when lengths of 2 hash sets do not match', function() {
+        var hashset1 = new HashSet();
+        var hashset2 = new HashSet(1);
+        var result = hashset1.equals(hashset2);
+        assert.strictEqual(result, false);
+    });
 
-	it('should return true if 2 hash sets have identical values', function() {
-		var hashset1 = new HashSet(1, 2, 3, 4, 5);
-		var hashset2 = new HashSet(1, 2, 3, 4, 5);
-		var result = hashset1.equals(hashset2);
-		assert.strictEqual(result, true);
-		result = hashset2.equals(hashset1);
-		assert.strictEqual(result, true);
-	});
+    it('should return true if 2 hash sets have identical values', function() {
+        var hashset1 = new HashSet(1, 2, 3, 4, 5);
+        var hashset2 = new HashSet(1, 2, 3, 4, 5);
+        var result = hashset1.equals(hashset2);
+        assert.strictEqual(result, true);
+        result = hashset2.equals(hashset1);
+        assert.strictEqual(result, true);
+    });
 
-	it('should return false if 2 hash set have different values', function() {
-		var hashset1 = new HashSet(1, 2, 6, 4, 5);
-		var hashset2 = new HashSet(1, 2, 3, 4, 5);
-		var result = hashset1.equals(hashset2);
-		assert.strictEqual(result, false);
-		result = hashset2.equals(hashset1);
-		assert.strictEqual(result, false);
-	});
+    it('should return false if 2 hash set have different values', function() {
+        var hashset1 = new HashSet(1, 2, 6, 4, 5);
+        var hashset2 = new HashSet(1, 2, 3, 4, 5);
+        var result = hashset1.equals(hashset2);
+        assert.strictEqual(result, false);
+        result = hashset2.equals(hashset1);
+        assert.strictEqual(result, false);
+    });
 });
 
 describe('#unionWith', function() {
-	it('should remain unchanged when union with an empty hash set', function() {
-		var hashset1 = new HashSet(1, 2, 3);
-		var hashset2 = new HashSet();
-		hashset1.unionWith(hashset2);
-		assert.strictEqual(hashset1.length, 3);
-	});
+    it('should remain unchanged when union with an empty hash set', function() {
+        var hashset1 = new HashSet(1, 2, 3);
+        var hashset2 = new HashSet();
+        hashset1.unionWith(hashset2);
+        assert.strictEqual(hashset1.length, 3);
+    });
 
-	it('should merge 2 hash sets', function() {
-		var hashset1 = new HashSet(1, 2);
-		var hashset2 = new HashSet(2, 3);
-		hashset1.unionWith(hashset2);
-		assert.strictEqual(hashset1.length, 3);
-		assert.strictEqual(hashset1.contains(1), true);
-		assert.strictEqual(hashset1.contains(2), true);
-		assert.strictEqual(hashset1.contains(3), true);
-	});
+    it('should merge 2 hash sets', function() {
+        var hashset1 = new HashSet(1, 2);
+        var hashset2 = new HashSet(2, 3);
+        hashset1.unionWith(hashset2);
+        assert.strictEqual(hashset1.length, 3);
+        assert.strictEqual(hashset1.contains(1), true);
+        assert.strictEqual(hashset1.contains(2), true);
+        assert.strictEqual(hashset1.contains(3), true);
+    });
 });
